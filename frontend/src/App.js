be0b0f0e0169d88d2aff3286f1498c01a831ff7e
Jitendra-civil <jitendrapanigrahi16@gmail.com>
2024-05-07 18:58:14 +0530

    firstCommit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Student from './student.js';
+import CreateStudent from './CreateStudent.js';
+import UpdateStudent from './UpdateStudent.js';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route exact path='/' Component={Student}/>
+        <Route path = '/create' element= {<CreateStudent/>}></Route>
+        <Route path = '/update/:id' element= {<UpdateStudent/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
